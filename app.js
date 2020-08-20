8e874152eca71740eafc0a653df3af6d250e5fec
leahyjsamuel <68017098+leahyjsamuel@users.noreply.github.com>
2020-08-20 17:49:43 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+let q1 = prompt("Am I male or female?")
+
+if (q1.toLocaleLowerCase() ==="female") {
+    alert("Correct!");
+} else {
+    alert("Incorrect!");
+}
+
+let q2 = prompt("Am I from the UK or USA?")
+if (q2.toLocaleUpperCase() ==="USA") {
+    alert("Correct!");
+} else {
+    alert("Incorrect!");
+}
+
+
+let q3 = prompt("Who am I?")
+if (q3 ==="Beyonce") {
+alert("Correct! I am the Queen Bey!");
+} else {
+    alert("Incorrect!");
+}
